996809b17a1d58a4adcbf0dd1674345af5c45138
SuperTuxii <123881249+SuperTuxii@users.noreply.github.com>
2025-12-21 21:24:40 +0100

    Created the standard setup for a 2d side scroller. There is a demo level, the character can be moved and I also separated the background part from the main menu background. The player/character also uses the animation that I created previously (with more needed for the future).

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,3 +7,6 @@ func _on_start_button_pressed() -> void:
 
 func _on_options_button_pressed() -> void:
 	pass # TODO: Add Options
+
+func _on_start_animation_animation_finished() -> void:
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
