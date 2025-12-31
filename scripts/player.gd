7cf12bf9f33de89222917f3329ed510f0c8d4af1
SuperTuxii <123881249+SuperTuxii@users.noreply.github.com>
2026-01-01 00:47:39 +0100

    Adding various things for when the player is afk (idk why I did this now. Maybe because I'm too lazy to work on more important stuff). There is an animation that should look like the players leaves are getting blown by the wind and if the player is afk for too long then the player will go back into the ground.

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ signal moved(difference: Vector2)
 
 @onready
 var texture: AnimatedSprite2D = $Texture
+@onready
+var timer: Timer = $Timer
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -350.0
@@ -17,6 +19,15 @@ var jump_amplifier: float = 1.0
 
 func standup() -> void:
 	texture.play("standup")
+
+func bury() -> void:
+	texture.play_backwards("standup")
+
+func idle() -> void:
+	texture.play("idle")
+	texture.sprite_frames.set_frame("idle", 0, texture.sprite_frames.get_frame_texture("idle", 0), randi_range(20, 50))
+	if timer.is_stopped():
+		timer.start(randf_range(50, 100))
 
 func add_item(item_id: StringName, amount: int = 1):
 	inventory[item_id] = inventory.get(item_id, 0) + amount
@@ -33,14 +44,17 @@ func remove_item(item_id: StringName, amount: int = 1):
 func _physics_process(delta: float) -> void:
 	if not is_zero_approx(velocity.x):
 		texture.play("walk")
-	elif texture.animation != "standup":
-		texture.play("idle")
+	elif texture.animation == "walk":
+		idle()
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if texture.animation != "standup":
+	if texture.animation == "burried":
+		if Input.is_action_just_pressed("up") or Input.is_action_just_pressed("right") or Input.is_action_just_pressed("down") or Input.is_action_just_pressed("left"):
+			standup()
+	elif texture.animation != "standup":
 		# Handle jump.
 		if Input.is_action_just_pressed("up") and is_on_floor():
 			velocity.y = JUMP_VELOCITY * jump_amplifier
@@ -59,4 +73,18 @@ func _physics_process(delta: float) -> void:
 
 func _on_texture_animation_finished() -> void:
 	if texture.animation == "standup":
-		texture.play("idle")
+		if texture.frame != 0:
+			idle()
+		else:
+			texture.play("burried")
+	else:
+		idle()
+
+func _on_texture_animation_changed() -> void:
+	if texture == null or timer == null:
+		return
+	if texture.animation != "idle":
+		timer.stop()
+
+func _on_timer_timeout() -> void:
+	bury()
